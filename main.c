e7a8ac2fae759163b99e9bf7e423938c59d40624
GOLALE-wild <93827885+GOLALE-wild@users.noreply.github.com>
2022-03-02 21:30:28 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void perevod(int a) {
+    int dvoich[20], k = 0;
+    while (a > 0) {
+        dvoich[k] = (a % 2);
+        k++;
+        a = a / 2;
+    }
+    for (k; k > 0; k--) {
+        printf("%d", dvoich[k]);
+    }
+}
+int main(void) {
+    int mas[99], n, i = 0;
+    while ((scanf("%d", &n)) && (n != EOF)) {
+        mas[i] = n;
+        i++;
+    }
+    for (int m = 0; m < i; m++) {
+        printf("%d ", mas[m]);
+    }
+    printf("\n");
+    for (int m = 0; m < i; m++) {
+        perevod(mas[m]);
+        printf(" ");
+    }
+    return 0;
+}
